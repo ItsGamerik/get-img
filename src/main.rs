01415779ee1ffcf552d34c1a5545374d736a9bc7
ItsGamerik <erschmia@gmail.com>
2023-06-23 23:14:49 +0200

    remove unnecessary gateway intent

--- src/main.rs
+++ src/main.rs
@@ -65,7 +65,6 @@ impl EventHandler for Handler {
 async fn main() {
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
     let intents = GatewayIntents::GUILD_MESSAGES
-        | GatewayIntents::DIRECT_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT;
 
     let mut client = Client::builder(&token, intents)
