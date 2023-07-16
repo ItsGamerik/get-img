f2e181347905bb95d84161b35c971455f12a4621
ItsGamerik <erschmia@gmail.com>
2023-07-16 10:42:31 +0200

    modify activity using status_message()

--- src/helper_functions.rs
+++ src/helper_functions.rs
@@ -1,12 +1,13 @@
 // this file contains some helper functions
 
 use serenity::{
-    model::prelude::{application_command::ApplicationCommandInteraction, InteractionResponseType},
+    model::prelude::{application_command::ApplicationCommandInteraction, InteractionResponseType, Activity},
     prelude::Context,
 };
 
 /// used to create interaction responses.
 pub async fn status_message(ctx: &Context, msg: &str, interaction: &ApplicationCommandInteraction) {
+    start_action(ctx).await;
     interaction
         .create_interaction_response(&ctx.http, |response| {
             response
@@ -23,6 +24,7 @@ pub async fn edit_status_message(
     response_string: &str,
     interaction: &ApplicationCommandInteraction,
 ) {
+    stop_action(ctx).await;
     interaction
         .edit_original_interaction_response(&ctx.http, |response| response.content(response_string))
         .await
@@ -35,8 +37,17 @@ pub async fn followup_status_message(
     response_string: &str,
     interaction: &ApplicationCommandInteraction,
 ) {
+    stop_action(ctx).await;
     interaction
         .create_followup_message(&ctx.http, |response| response.content(response_string))
         .await
         .unwrap();
+}
+
+async fn start_action(ctx: &Context) {
+    ctx.set_activity(Activity::watching("currently working...")).await;
+}
+
+async fn stop_action(ctx: &Context) {
+    ctx.set_activity(Activity::watching("v.1.2")).await;
 }
