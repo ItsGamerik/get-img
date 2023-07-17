59d5c987db7f6b0a96bfd772bb7c7e04dc932619
ItsGamerik <erschmia@gmail.com>
2023-07-17 12:55:20 +0200

    this is gonna be a mess, but it is necessary

--- src/helper_functions.rs
+++ src/helper_functions.rs
@@ -1,7 +1,16 @@
 // this file contains some helper functions
 
+use std::fs::{self, OpenOptions};
+use std::io::Write;
+
 use serenity::{
-    model::{prelude::{application_command::ApplicationCommandInteraction, InteractionResponseType, Activity}, user::OnlineStatus},
+    model::{
+        prelude::{
+            application_command::ApplicationCommandInteraction, Activity, Attachment,
+            InteractionResponseType, Message,
+        },
+        user::OnlineStatus,
+    },
     prelude::Context,
 };
 
@@ -47,10 +56,51 @@ pub async fn followup_status_message(
 async fn start_action(ctx: &Context) {
     // the status update seems to be very slow, so it probably wont actually show unless a task is started that takes a long time
     let status = OnlineStatus::DoNotDisturb;
-    ctx.set_presence(Some(Activity::watching("currently working...")), status).await;
+    ctx.set_presence(Some(Activity::watching("currently working...")), status)
+        .await;
 }
 
 async fn stop_action(ctx: &Context) {
     let status = OnlineStatus::Online;
-    ctx.set_presence(Some(Activity::watching("v1.2 - ready for work")), status).await;
+    ctx.set_presence(Some(Activity::watching("v1.2 - ready for work")), status)
+        .await;
 }
+
+// used to parse messages into the output.
+// does this one message at a time.
+// TODO: make this
+// pub async fn universal_parser(message: Message, only_attachments: bool) {
+//     let message_author = message.author;
+//     let message_timestamp = message.timestamp;
+//     let message_content = message.content;
+//     let message_attachments: Vec<&Attachment> = message.attachments.iter().collect();
+
+//     if let Err(e) = fs::create_dir_all("./download/") {
+//         eprintln!("error creating download file: {}", e);
+//     }
+
+//     let mut file = match OpenOptions::new()
+//         .write(true)
+//         .create(true)
+//         .append(true)
+//         .open("./download/output.txt")
+//     {
+//         Ok(file) => file,
+//         Err(e) => {
+//             eprintln!("could not open file \"output.txt\": {}", e);
+//             return;
+//         }
+//     };
+
+//     if only_attachments == true {
+//         for i in message_attachments {
+
+//         }
+//     } else {
+//         let formatted_message: String = format!("{}, \"{}\", {}", message_author, message_content, message_timestamp);
+//     }
+
+//     if let Err(e) = writeln!(file, "{formatted_message}") {
+//         eprintln!("error writing to file output.txt: {}", e);
+//     }
+// }
