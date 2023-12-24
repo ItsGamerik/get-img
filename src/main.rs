9583d271031645dfab49688bde30a75b51b50daf
ItsGamerik <erschmia@gmail.com>
2023-12-24 17:29:31 +0100

    reimplement help command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use log::{error, info, LevelFilter, warn};
+mod commands;
+
+use log::{error, info, warn, LevelFilter};
+use serenity::all::{GuildId, Interaction};
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
@@ -10,17 +13,56 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        warn!("not implemented!")
+        todo!()
     }
-    async fn ready(&self, _: Context, ready: Ready) {
-        info!("bot \"{}\" has started", ready.user.name)
+
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        info!("bot \"{}\" has started", ready.user.name);
+
+        let guild_id = GuildId::new(env::var("GUILD_ID").unwrap().parse().unwrap());
+
+        if let Err(e) = guild_id
+            .set_commands(&ctx.http, vec![commands::help::register()])
+            .await
+        {
+            error!("error creating one or more commands: {e}")
+        };
+    }
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::Command(command) = interaction {
+            info!("recieved commands interaction");
+
+            let _ = match command.data.name.as_str() {
+                "help" => {
+                    commands::help::run(ctx, &command).await;
+                    Some(())
+                }
+                _ => Some(warn!(
+                    "command not implemented: {}",
+                    format!("{}", command.data.name)
+                )),
+            };
+
+            // if let Some(content) = content {
+            //     let data = CreateInteractionResponseMessage::new().content(content);
+            //     let builder = CreateInteractionResponse::Message(data);
+            //     if let Err(why) = command.create_response(&ctx.http, builder).await {
+            //         println!("Cannot respond to slash command: {why}");
+            //     }
+            // }
+        }
     }
 }
 
-
 #[tokio::main]
 async fn main() {
-    SimpleLogger::new().with_colors(true).with_level(LevelFilter::Off).with_module_level("get_img", LevelFilter::Info).init().unwrap();
+    SimpleLogger::new()
+        .with_colors(true)
+        .env()
+        .with_level(LevelFilter::Off)
+        .with_module_level("get_img", LevelFilter::Info)
+        .init()
+        .unwrap();
     let token: String = match env::var("DISCORD_TOKEN") {
         Ok(token) => {
             info!("token found in env!");
@@ -34,7 +76,10 @@ async fn main() {
 
     let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
 
-    let mut client = Client::builder(&token, intents).event_handler(Handler).await.unwrap();
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .unwrap();
 
     if let Err(e) = client.start().await {
         error!("an error occured starting client: {e}");
